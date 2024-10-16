06b0100f775028dd7cf0cef689006bfccf7ec27f
Sam Schams <samschams@gmail.com>
2024-10-16 18:58:47 +0200

    add noticed gem; set associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { minimum: 5, maximum: 50 }
     validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
 
+    has_many :noticed_events, as: :record, dependent: :destroy, class_name: "Noticed::Event"
+    has_many :notifications, through: :noticed_events, class_name: "Noticed::Notification"
+    
     belongs_to :user
     has_many :comments, dependent: :destroy
 end
