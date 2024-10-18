ea91d7285a0b0bf86693f5fbfb2a1d45c0ff195d
Sam Schams <samschams@gmail.com>
2024-10-18 20:08:10 +0200

    pass users notifications to view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_notifications, if: :current_user
+
+  private
+
+  def set_notifications
+    @unread_notifications = current_user.notifications.unread
+    @read_notifications = current_user.notifications.read
+  end
 end
