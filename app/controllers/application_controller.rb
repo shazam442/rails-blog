57d807e10322ddf7be841a98f3a47afbe8aa7e80
Sam Schams <samschams@gmail.com>
2024-10-20 20:35:50 +0200

    add search page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :set_notifications, if: :current_user
+  before_action :set_up_search_bar
 
   private
+
+  def set_up_search_bar
+    @query = Post.ransack(params[:query])
+  end
 
   def set_notifications
     @unread_notifications = current_user.notifications.unread
